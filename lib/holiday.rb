eb45e9c8ba65b8ad640393b93698216026493576
Sherzod Karimov <karimovj@gmail.com>
2018-07-23 17:23:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supply|
-      return holiday if supply.include?("BBQ")
+     holiday if supply.include?("BBQ")
   end
 end
 end
